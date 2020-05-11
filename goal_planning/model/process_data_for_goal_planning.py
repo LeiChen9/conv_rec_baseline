b66a4d73ca694a02e38b04d0cb57584ed4df1d34
riceball <chenlei9691@gmail.com>
2020-05-11 10:22:59 +0800

    Data processing half done

--- goal_planning/model/process_data_for_goal_planning.py
+++ goal_planning/model/process_data_for_goal_planning.py
@@ -3,6 +3,7 @@ import sys
 import json
 import collections
 import random
+import pdb
 
 
 def is_start_with(str, start_str=['[1]', '[2]', '[3]', '[4]', '[5]', '[6]', '[7]', '[8]']):
@@ -50,6 +51,13 @@ def process_session_data(input_filename, output_filename):
         situation = data['situation']
         conversation = data['conversation']
         goals = data['goal']
+        """
+        goals like: [1] 问 日期 ( User 主动 问 日期 ， Bot 根据 『 参考 知识 』 的 『 聊天   日期 』 回答 ， 然后 User 满足 并 好评 ) 
+        --> [2] 关于 明星 的 聊天 ( Bot 主动 ， 从 『 周杰伦 』 的 生日 开始 聊 ， 根据 给定 的 明星 信息 聊   『 周杰伦 』   相关 内容 
+        ， 至少 要 聊 2 轮 ， 避免 话题 切换 太 僵硬 ， 不够 自然 ) --> [3] 音乐 推荐 ( Bot 主动 ， Bot 使用   『 免费教学录像带 』   
+        的 某个 评论 当做 推荐 理由 来 推荐   『 免费教学录像带 』 ， User 接受 。 需要 聊 2 轮 ) --> [4] 播放 音乐 ( Bot 主动 询问 是否
+         播放 ， User 同意 后 ， Bot 播放   『 免费教学录像带 』 ) --> [5] 再见
+        """
         label = data['label']
         kg = data['knowledge']
         user_profile = data['user_profile']
@@ -65,6 +73,89 @@ def process_session_data(input_filename, output_filename):
             user_profile_key_result.add(key)
         for key in user_profile:
             keyword_list = p_p_key + p_a_key + p_r_key
+            if key not in keyword_list:
+                continue
+            tmp_entity = user_profile[key]
+            if isinstance(tmp_entity, list):
+                for k in tmp_entity:
+                    profile_entity_list.add(k.strip())
+            else:
+                profile_entity_list.add(tmp_entity.strip())
+        profile_entity_list = list(profile_entity_list)
+        if len(goals) != sum(label):
+            continue
+        first_goal = goals[0].strip().split(']', 1)[1].split('(', 1)[0]  # Need testing
+        # Set dialog flag
+        for i in range(len(label)):
+            if first_goal == '寒暄':
+                if i % 2 == 0:
+                    dialog_flag = 'Bot'
+                else:
+                    dialog_flag = 'User'
+            else:
+                if i % 2 != 0:
+                    dialog_flag = 'Bot'
+                else:
+                    dialog_flag = 'User'
+
+            if label[i] == 1:
+                current_goal = goals[count - 1].split('[{0}]'.format(count))[-1]  # remove label like [1], [2]
+                domain = current_goal.split('(', 1)[0]  # like ' 问 日期 '
+                if "『" and "』" not in current_goal:
+                    output_file.write(conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + domain + '\t' +\
+                                      str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                    current_entity_goal = domain
+                else:
+                    if domain in flag1:
+                        tmp = current_goal.split("『")[-1].split("』", 1)[0]
+                        if domain == "问日期":
+                            output_file.write(conversation[i] + '\t' + str(label[i]) + '\t' +domain + '\t' + domain + '\t'\
+                                              + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                            current_entity_goal = tmp + " 新闻"
+                        else:
+                            output_file.write(
+                                conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + domain + '\t' \
+                                + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                            current_entity_goal = tmp
+                    elif domain in flag2:
+                        tmp1 = current_goal.split("『", 1)[-1].split("』", 1)[0]
+                        tmp2 = current_goal.split("『", -1)[-1].split("』", -1)[0]
+                        if tmp1 not in bad_flag:
+                            if flag_flag == 0:
+                                output_file.write(
+                                    conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + tmp1 + '\t' \
+                                    + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                                current_entity_goal = tmp2
+                                flag_flag = 1
+                            else:
+                                output_file.write(
+                                    conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + tmp2 + '\t' \
+                                    + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                                current_entity_goal = tmp2
+                                flag_flag = 0
+                        else:
+                            if flag_flag == 1:
+                                output_file.write(
+                                    conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + current_entity_goal + '\t' \
+                                    + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                            else:
+                                output_file.write(
+                                    conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + tmp2 + '\t' \
+                                    + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                                current_entity_goal = tmp2
+                    else:
+                        output_file.write(
+                            conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + domain + '\t' \
+                            + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+                count += 1
+            else:
+                output_file.write(
+                    conversation[i] + '\t' + str(label[i]) + '\t' + domain + '\t' + current_entity_goal + '\t' \
+                    + str(kg) + '\t' + str(user_profile) + '\t' + dialog_flag + '\n')
+        output_file.write('\n')
+    output_file.close()
+
+
 
 
 if __name__ == "__main__":
@@ -72,4 +163,4 @@ if __name__ == "__main__":
     add_label('../origin_data/resource/dev.txt', '../origin_data/dev_add_label.txt')
     process_session_data('../origin_data/train_add_label.txt', '../origin_data/train.txt')
     process_session_data('../origin_data/dev_add_label.txt', '../origin_data/dev.txt')
-    process_sample_data('../origin_data/resource/test_1.txt', '../origin_data/test.txt')
+    # process_sample_data('../origin_data/resource/test_1.txt', '../origin_data/test.txt')
